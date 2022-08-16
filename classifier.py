4466afa72ba211d0350a8ad6674ee480027f179a
MNastri <marcellonastri@gmail.com>
2022-08-16 16:57:03 -0300

    doc:add type hint for classifier

--- classifier.py
+++ classifier.py
@@ -3,7 +3,9 @@ from os.path import join
 
 import joblib
 
+from sklearn.svm import SVC
+
 classifier_filename = "iris_classifier.joblib"
 basepath_of_classifier = getcwd()
 classifier_fullpath = join(basepath_of_classifier, classifier_filename)
-classifier = joblib.load(filename=classifier_fullpath)
+classifier: SVC = joblib.load(filename=classifier_fullpath)
